5974492a7465659183805cca568d3926b9ad2981
Nathaniel Pisarski <nathanpisarski@gmail.com>
2018-02-12 21:53:21 -0500

    Add the ability to use fast-options via take match

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import {head, filter, size, map} from 'lodash'
+
 const toCamelCase = (prefix, string) => {
     return ('' + prefix + (string.charAt(0).toUpperCase()) + string.substring(1, string.length))
 }
 
 const unsafeIsSetter = candidate => candidate && {}.toString.call(candidate) === '[object Function]'
 
+const first = (list, predicate) => head(filter(list, predicate))
+
+const stringArrayOperation = (string, operation) => operation(string.split('')).join('')
+
+const camelCaseInitialism = word => word.substring(0, 1) + stringArrayOperation(word, characters => filter(characters, character => character.toUpperCase() === character))
 
 const alienDefaults = {
     finalization: {
@@ -31,7 +38,43 @@ const alienDefaults = {
     }
 }
 
-export const ℿ = (builderArray, options) => {
+export const fastOption = (fastOption) => {
+    // {f: {ru: true, rs: true}, o: {r: false}}
+    // Step 1: Find first expansion of item
+    // Step 2: For each property, find first expansion and replace
+
+    const fastOptionKeys = Object.keys(fastOption)
+    const optionKeys = Object.keys(alienDefaults)
+
+    const findFirstTakeMatch = (keyList, fastOptionKey) => first(keyList, key => key.substring(0, size(fastOptionKey)) === fastOptionKey)
+    const camelCaseInitialismMatch = (keyList, fastOptionKey) => findFirstTakeMatch(map(keyList, camelCaseInitialism), fastOptionKey)
+
+    const test = (keyList, fastOptionKey) => findFirstTakeMatch(keyList, fastOptionKey) || camelCaseInitialismMatch(keyList, fastOptionKey)
+
+    let providedOptions = {}
+
+    // For each top-level key
+    for(const fastOptionKey of fastOptionKeys) {
+
+        // Get the alien option name for the top level
+        const topLevelKey = test(optionKeys, fastOptionKey)
+        let topLevelOptions = {}
+
+        const innerKeys = Object.keys(fastOption[fastOptionKey])
+
+        for(const innerFastOptionKey of innerKeys) {
+            const innerOptions = Object.keys(alienDefaults[[topLevelKey]])
+            const selectedOption = test(innerOptions, innerFastOptionKey)
+            topLevelOptions[[selectedOption]] = fastOption[[fastOptionKey]][[innerFastOptionKey]]
+        }
+        providedOptions[[topLevelKey]] = topLevelOptions
+    }
+
+    return providedOptions
+}
+
+export const ℿ = (builderArray, alienOptions) => {
+    let options = (alienOptions && fastOption(alienOptions)) || alienDefaults
     const getValueOrDefault = (section, ident) => (options && options[section] && (options[section][ident] !== null && options[section][ident] !== undefined)) && options[section][ident]
         || alienDefaults[section][ident]
 
